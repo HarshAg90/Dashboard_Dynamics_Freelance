bcbc6820518adf01fe2e7e3245ef51ccda357816
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-07-31 16:17:54 +0530

    home page statically done

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -75,8 +75,8 @@ variants.visible = { ...variants.visible, background: backgroundColor };
             <Link to="/"><li className="nav_elements logo"><img src="../assets/logo.png" alt="" /></li></ Link>
             <Link to="/"><li className="nav_elements">Home</li></ Link>
             <Link to="/Technology"><li className="nav_elements">Technology</li></ Link>
-            <Link to="/Products"><li className="nav_elements">Our Produts</li></ Link>
-            <Link to="/Use_cases"><li className="nav_elements">Use Case</li></ Link>
+            <Link to="/Products"><li className="nav_elements">Our Products</li></ Link>
+            <Link to="/Use_case"><li className="nav_elements">Use Cases</li></ Link>
             <Link to="/Contacts"><li className="nav_elements">Contact</li></ Link>
             <Link to="/News_rooms"><li className="nav_elements">News Room</li></ Link>
             <Link to="/Careers"><li className="nav_elements">Career</li></ Link>

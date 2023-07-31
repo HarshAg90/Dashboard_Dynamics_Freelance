63fd7c2469fac13038954c0a7449caf9ff8b60de
lakshay gupta <lakshaygupta2511@gmail.com>
2023-07-31 14:40:52 +0530

    Charge page fixed

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -76,7 +76,7 @@ variants.visible = { ...variants.visible, background: backgroundColor };
             <Link to="/"><li className="nav_elements">Home</li></ Link>
             <Link to="/Technology"><li className="nav_elements">Technology</li></ Link>
             <Link to="/Products"><li className="nav_elements">Our Produts</li></ Link>
-            <Link to="/Use_cases"><li className="nav_elements">Use Case</li></ Link>
+            <Link to="/Use_case"><li className="nav_elements">Use Case</li></ Link>
             <Link to="/Contacts"><li className="nav_elements">Contact</li></ Link>
             <Link to="/News_rooms"><li className="nav_elements">News Room</li></ Link>
             <Link to="/Careers"><li className="nav_elements">Career</li></ Link>

2bb9a5445a2b49cf4ff0d041357e993ce9ab5b76
lakshay gupta <lakshaygupta2511@gmail.com>
2023-08-11 22:01:36 +0530

    Added line animation in Tech Page

--- src/components/CarCanvas.jsx
+++ src/components/CarCanvas.jsx
@@ -21,7 +21,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 2 : 2.5}
-        position={isMobile ? [0, -3, -2.2] : [0, -3, -1.5]}
+        position={isMobile ? [0, -3, -2.2] : [0, -3, 0]}
         // rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
